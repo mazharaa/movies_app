14cd3b6dc5ab4a79bc40591daa88326bf2de0c56
azhar <mazharaa32@gmail.com>
2024-08-06 00:49:24 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(const MoviesApp());
+}
+
+class MoviesApp extends StatelessWidget {
+  const MoviesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
