d2b2266437161c58ac5a152c07147f805f0c0bce
azhar <mazharaa32@gmail.com>
2024-08-09 06:02:16 +0700

    Configuring AppRouter

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,29 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:movies_app/core/injection/injection.dart';
+import 'package:movies_app/core/routes/app_router.dart';
 
 void main(List<String> args) {
-  runApp(const MoviesApp());
+  runApp(MoviesApp());
 }
 
 class MoviesApp extends StatelessWidget {
-  const MoviesApp({super.key});
+  MoviesApp({super.key});
+
+  final router = getIt<AppRouter>();
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return ScreenUtilInit(
+      designSize: const Size(375, 812),
+      builder: (context, child) {
+        return MaterialApp.router(
+          title: 'MoviesApp',
+          routerDelegate: AutoRouterDelegate(router),
+          routeInformationParser: router.defaultRouteParser(),
+        );
+      },
+    );
   }
 }
