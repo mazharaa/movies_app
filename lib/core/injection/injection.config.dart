a852e0aa5c17c0cf997882446fcf2e219c6b4c90
azhar <mazharaa32@gmail.com>
2024-08-09 06:01:47 +0700

    Register AppRouter as module

--- lib/core/injection/injection.config.dart
+++ lib/core/injection/injection.config.dart
@@ -12,6 +12,7 @@ import 'package:dio/dio.dart' as _i361;
 import 'package:get_it/get_it.dart' as _i174;
 import 'package:injectable/injectable.dart' as _i526;
 import 'package:movies_app/core/env/env.dart' as _i415;
+import 'package:movies_app/core/routes/app_router.dart' as _i387;
 import 'package:movies_app/core/utils/register_module.dart' as _i525;
 import 'package:movies_app/domain/home/i_home_repository.dart' as _i1028;
 import 'package:movies_app/infrastructure/core/api_helper.dart' as _i725;
@@ -33,6 +34,7 @@ extension GetItInjectableX on _i174.GetIt {
     );
     final registerModule = _$RegisterModule();
     gh.factory<_i415.Env>(() => _i415.Env());
+    gh.singleton<_i387.AppRouter>(() => registerModule.appRouter);
     gh.factory<_i361.Dio>(() => registerModule.dio(gh<_i415.Env>()));
     gh.lazySingleton<_i725.ApiHelper>(() => _i725.ApiHelper(gh<_i361.Dio>()));
     gh.factory<_i651.HomeDataSources>(
--- lib/core/utils/register_module.dart
+++ lib/core/utils/register_module.dart
@@ -1,9 +1,13 @@
 import 'package:dio/dio.dart';
 import 'package:injectable/injectable.dart';
 import 'package:movies_app/core/env/env.dart';
+import 'package:movies_app/core/routes/app_router.dart';
 
 @module
 abstract class RegisterModule {
+  @singleton
+  AppRouter get appRouter => AppRouter();
+
   Dio dio(Env env) {
     Dio _dio = Dio();
     BaseOptions options = BaseOptions(
