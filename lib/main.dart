fd1794027169249c35737d0f8a7da7beb67fc632
azhar <mazharaa32@gmail.com>
2024-08-11 12:45:44 +0700

    Add FavWatchlistCubit as BlocProvider

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:movies_app/application/fav_watchlist/fav_watchlist_cubit.dart';
 import 'package:movies_app/application/nav_bar/nav_bar_cubit.dart';
 import 'package:movies_app/core/common/app_theme.dart';
 import 'package:movies_app/core/injection/injection.dart';
@@ -24,6 +25,9 @@ class MoviesApp extends StatelessWidget {
         BlocProvider(
           create: (context) => NavBarCubit(),
         ),
+        BlocProvider(
+          create: (context) => FavWatchlistCubit(),
+        )
       ],
       child: ScreenUtilInit(
         designSize: const Size(375, 812),
