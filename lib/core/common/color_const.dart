80f2fb2e0a08b86cef9010ca9c082be1676c439b
azhar <mazharaa32@gmail.com>
2024-09-25 08:03:22 +0700

    Add darkGrey color to ColorConst

--- lib/core/common/color_const.dart
+++ lib/core/common/color_const.dart
@@ -3,11 +3,12 @@ import 'package:flutter/material.dart';
 class ColorConst {
   ColorConst._();
 
-  static const Color black = Color(0xFF242A32);
+  static const Color black = Color(0xFF131313);
   static const Color white = Color(0xFFFFFFFF);
   static const Color blue = Color(0xFF0296E5);
   static const Color grey = Color(0xff898989);
   static const Color fontGrey = Color(0xFF92929D);
-  static Color lightBlue = const Color(0xFF96D3F2).withOpacity(0.2);
+  static const Color lightBlue = Color(0xFFACCEF7);
   static const Color lightGrey = Color(0xffD9D9D9);
+  static const Color darkGrey = Color(0xff333333);
 }
